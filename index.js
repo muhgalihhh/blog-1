20ff2169d339fe9852dc5c629ec005b8d67d28a2
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-01-31 14:10:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+let blogs = [
+  {
+    id: "1",
+    title: "Blog Satu",
+    snippet:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptatibus!",
+  },
+  {
+    id: "2",
+    title: "Blog Dua",
+    snippet:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptatibus!",
+  },
+  {
+    id: "3",
+    title: "Blog Tiga",
+    snippet:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptatibus!",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { blogs: blogs });
+});
+
+app.get("/blogs", (req, res) => {
+  res.render("blogs/index.ejs", { blogs: blogs });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("blogs/create.ejs");
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  const blog = blogs.find((blog) => blog.id == id);
+  res.render("blogs/details.ejs", { blog: blog });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  blogs = blogs.filter((blog) => blog.id != id);
+  res.json({ redirect: "/blogs" });
+});
+
+app.put("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, snippet } = req.body;
+  const blog = blogs.find((blog) => blog.id === id);
+  if (blog) {
+    blog.title = title;
+    blog.snippet = snippet;
+  }
+  res.render("blogs/details.ejs", { blog: blog });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
